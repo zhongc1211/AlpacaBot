df5af84f863921e4bad3f2fdf6300ceaaf5d2555
zack <zack.zhong@sleek.com>
2021-05-28 16:30:57 +0800

    no run

--- bot.py
+++ bot.py
@@ -57,10 +57,11 @@ if __name__ == "__main__":
     print(os.getenv('INPUT_PRAW_CLIENT_ID'))
     print(os.getenv('INPUT_PRAW_CLIENT_SECRET'))
     print(os.getenv('INPUT_PRAW_USER_AGENT'))
-    reddit=praw.Reddit(client_id=os.getenv('PRAW_CLIENT_ID'),
-                        client_secret=os.getenv('PRAW_CLIENT_SECRET'),
-                        user_agent=os.getenv('PRAW_USER_AGENT'),
-                        username='JustAnAlpacaBot',
-                        password=os.getenv('PRAW_PASSWORD'))
+    print('env loaded')
+    # reddit=praw.Reddit(client_id=os.getenv('PRAW_CLIENT_ID'),
+    #                     client_secret=os.getenv('PRAW_CLIENT_SECRET'),
+    #                     user_agent=os.getenv('PRAW_USER_AGENT'),
+    #                     username='JustAnAlpacaBot',
+    #                     password=os.getenv('PRAW_PASSWORD'))
     
-    main(reddit)
+    # main(reddit)
